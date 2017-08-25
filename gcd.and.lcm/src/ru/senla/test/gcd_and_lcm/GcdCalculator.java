575487461b3f557a114f7b872454709f21cee571
Yury Vysocki <drigexe@gmail.com>
2017-08-25 13:05:42 +0300

    saving changes in gcd

--- gcd.and.lcm/src/ru/senla/test/gcd_and_lcm/GcdCalculator.java
+++ gcd.and.lcm/src/ru/senla/test/gcd_and_lcm/GcdCalculator.java
@@ -1,5 +1,12 @@
 package ru.senla.test.gcd_and_lcm;
 
 public class GcdCalculator {
+	public int gcdCalculate(int firstNumber, int secondNumber) {
+		if (secondNumber == 0) {
+			return firstNumber;
+		} else {
+			return gcdCalculate(secondNumber, firstNumber % secondNumber);
+		}
+	}
 
 }
